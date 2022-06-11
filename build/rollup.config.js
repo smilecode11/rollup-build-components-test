610c7d05d6b93ba0cf00156abdca02e879b1a72a
尘心 <smilewupeng@gmail.com>
2022-06-11 17:01:29 +0800

    fix: 打包生成包(scoped)问题

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,23 +2,26 @@ import vue from 'rollup-plugin-vue'
 import css from 'rollup-plugin-css-only'
 import typescript from 'rollup-plugin-typescript2'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import { name } from '../package.json'
-const file = type => `dist/${name}.${type}.js`
+// import { name } from '../package.json'
+
+const packageName = `lego-bricks`
+const file = type => `dist/${packageName}.${type}.js`
 
 const overrides = {
     compilerOptions: {
         declaration: true
     },
     exclude: [
-        "src/main.ts",
+        "tests/**/*.ts",
+        "tests/**/*.tsx"
     ]
 }
 
-export { name, file }
+export { packageName as name, file }
 export default {
     input: 'src/index.ts',
     output: {
-        name,
+        name: packageName,
         file: file('esm'),
         format: 'es'
     },
